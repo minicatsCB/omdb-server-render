ec6fd497ef6e5172a6c4dac73e0cc71aac77039f
Maribel <minicatsCB@users.noreply.github.com>
2019-05-14 16:28:53 +0200

    Set EJS as view engine

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
 app.listen(port, () => console.log("Listening on port " + port));
