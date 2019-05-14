349e5b428b26464e6d71487bdd89ce0525bb198a
Maribel <minicatsCB@users.noreply.github.com>
2019-05-14 16:35:21 +0200

    Render index view with search bar when at root path

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.listen(port, () => console.log("Listening on port " + port));
